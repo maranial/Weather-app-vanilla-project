96fb1c0e65edf33e43c9d375825f7c448c9c49cb
Maranial18 <42255916+maranial@users.noreply.github.com>
2021-02-06 21:08:31 -0500

    Created functions including JS timestamp. Connect the HTML temp ids and JS functionalitiies

--- src/script.js
+++ src/script.js
@@ -1,3 +1,17 @@
+function showMonthAndDate(timestamp){
+let now = new Date(timestamp);
+let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+let month = months[now.getMonth()];
+let date = now.getDate();
+//console.log(`${month} ${date}`);
+return `${month} ${date}`;
+}
+
+
+
+
+
+
 function showTemperature(response){
     
     let temp = document.querySelector("#temperatureId");
@@ -6,19 +20,21 @@ function showTemperature(response){
     let feelsLikeCondition = document.querySelector("#feels-like-id");
     let highCondition = document.querySelector("#high-id");
     let lowCondition = document.querySelector("#low-id");
+    let currentDate = document.querySelector("#now");
     temp.innerHTML = Math.round(response.data.main.temp);
     city.innerHTML = response.data.name;
     description.innerHTML = response.data.weather[0].description;
     feelsLikeCondition.innerHTML = Math.round(response.data.main.feels_like);
     highCondition.innerHTML = Math.round(response.data.main.temp_max);
     lowCondition.innerHTML = Math.round(response.data.main.temp_min);
-
+    currentDate.innerHTML = showMonthAndDate(response.data.dt * 1000);
 }
 
 
 //give an weather API 
 let apiKey = "60b3896c2d6cd3626ba92846df62d601";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Boston&appid=${apiKey}&units=metric`;
+let city = "Boston";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 //console.log(apiUrl);
 axios.get(apiUrl).then(showTemperature);
